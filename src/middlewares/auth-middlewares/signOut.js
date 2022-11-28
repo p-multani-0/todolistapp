111cf6e1121f8bb84f5b722cd11745c85ce56615
p.multani1 <p.multani@pinv.it>
2022-11-28 09:25:14 +0100

    fix logout

--- src/middlewares/auth-middlewares/signOut.js
+++ src/middlewares/auth-middlewares/signOut.js
@@ -1,6 +1,10 @@
 const signOut = (req, res) => {
-    req.logOut()
-    return res.status(200).json({ message: 'Logged Out' })
-}
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+  });
+  return res.status(200).json({ message: "Logged Out" });
+};
 
 module.exports = signOut;
